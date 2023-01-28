f08be9c9760e1698387b329af34ea614f1407e73
Caroline Souza <loh_jg@hotmail.com>
2023-01-28 00:07:45 -0300

    so tem letra

--- src/desafios/teste.java
+++ src/desafios/teste.java
@@ -1,10 +1,30 @@
 package desafios;
 
+import java.util.Scanner;
+
 public class teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+		
+		int start;
+		int end ;
+		
+		boolean sohLetras = S.matches("[a-zA-Z]");
+		while(!sohLetras) {
+			System.out.println("Digite uma letra");
+			S = in.next();
+			sohLetras = S.matches("[a-zA-Z]");
+		
+		}
+		
+		
+		
+	
+		 
+		
 	}
 
 }
